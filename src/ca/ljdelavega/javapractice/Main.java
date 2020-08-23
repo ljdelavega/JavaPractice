8efacb71a4599b53306938c4ddcbc6444bcb7694
ljdelavega <lester.delavega@gmail.com>
2020-08-22 20:38:07 -0600

    Initial IntelliJ project creation

---
+++ src/ca/ljdelavega/javapractice/Main.java
@@ -0,0 +1,9 @@
+package ca.ljdelavega.javapractice;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello World!");
+    }
+}
